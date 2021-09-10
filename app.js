6296ce8785ed667f9fddba532d33a3979e4ace80
klefcodes <abrahamugbeshe@gmail.com>
2021-09-10 13:43:47 +0100

    docs: setup swagger for api documentation

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 // Routes
-const indexRouter = require("./routes");
 const questionRouter = require("./routes/question");
 const categoryRouter = require("./routes/category");
 
@@ -18,7 +19,6 @@ app.use(
   })
 );
 
-app.use("/", indexRouter);
 app.use("/api/questions", questionRouter);
 app.use("/api/category", categoryRouter);
 
@@ -32,5 +32,32 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+const docsOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Examify API",
+      version: "1.0.0",
+      description:
+        "Backend interview task to build a api that performs request for examination",
+    },
+    servers: [
+      {
+        url: `http://127.0.0.1:${process.env.PORT || 3333}`,
+        description: "Development server",
+      },
+      {
+        url: `http://examify.tech`,
+        description: "Production server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJSDoc(docsOptions);
+
+app.use("/", swaggerUI.serve, swaggerUI.setup(specs));
 
 module.exports = app;
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-
-router.get("/", function (req, res, next) {
-  res.json({ msg: "Welcome to the exam api" });
-});
-
-module.exports = router;
